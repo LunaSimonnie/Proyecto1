1f0f4a7116b722f99fe2eb599fd0e7fc3b46dca5
LunaSimonnie <lunasimonne@gmail.com>
2024-09-06 08:05:47 -0600

    En la práctica 1 tuvimos el proyecto, el segundo funciones normales, back box, las columnas no se amontonen, imágenes y texto desde carpetas

--- app/src/main/java/Clases/Variables.kt
+++ app/src/main/java/Clases/Variables.kt
@@ -20,11 +20,14 @@ import kotlinx.coroutines.newSingleThreadContext
 
         //Array
         val names= arrayOf("Erick", "Silvia", "Hector", "Gabriela")
+        val numeros= arrayOf(1,2,3,4,5,6,7,8,9)
 
         println(age)
     println("Welcome $name to your first kotlin project")
     println(add())
     println(product (x=10 , y=92))
+    println(imprimirArray(names))
+
     }
 fun add(): Int{ // hay que especificar lo que se va a retornar (Int)
     val x:Int = 5
@@ -34,5 +37,47 @@ fun add(): Int{ // hay que especificar lo que se va a retornar (Int)
 }
 fun product(x:Int, y:Int):Int {
    return x + y
+
 }
+
+fun imprimirArray(names:Array<String>){
+    for(name in names) {
+        println("Hello $name ")
+    }
+}
+// funcion que de un array de 9 num me diga cual es par e impar
+fun arrayParImpar(numeros:Array<Int>){
+    for(numero in numeros){
+        if(numero%2==0){
+            println("$numero Es par ")
+        }
+        else{
+            println("$numero Es impar ")
+        }
+    }
+}
+
+fun getDay(day:Int):String {
+    var name=""
+
+    when(day){
+        1-> name="Monday"
+        2-> name="Tuesday"
+        3-> name="Wednesday"
+        4-> name="Thursday"
+        5-> name="Friday"
+        6-> name="Saturday"
+        7-> name="Sunday"
+        else ->name ="Incorrect Value"
+    }
+
+    return name
+}
+
+class Person (val name: String, val age:Int){
+    fun displayInformation(){
+        println("Name: $name,Age : $age")
+    }
+}
+
 //}
--- app/src/main/java/com/example/proyect1/MainActivity.kt
+++ app/src/main/java/com/example/proyect1/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.example.proyect1
 
+import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyect1.ui.theme.Proyect1Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,17 +42,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Proyect1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Column(
+                modifier= Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                Text(text ="Simple text")
+                ModifierExample()
+                ModifierExample2()
+                ModifierExample3()
+
             }
+            //Layouts
+            /*Column {
+                 Text(text = "First Row")
+                 Text(text = "Second Row")
+                 Text(text = "Third Row")
+
+                 Row{
+                     Text(text = "Text1")
+                     Text(text = "Text2")
+                     Text(text = "Text3")
+                     Text(text = "Text2")
+                 }
+                 Box{
+                     Text(text = "Larabel 1")
+                     Text(text = "Larabel 2")
+
+                 }
+                 Greeting(name ="World")
+             }*/
         }
     }
 }
+
+private fun column(function: () -> Unit) {
+
+}
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -44,4 +97,95 @@ fun GreetingPreview() {
     Proyect1Theme {
         Greeting("Android")
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier= Modifier
+            .padding(24.dp)
+    ){
+        Text(text="Hello world")
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+
+fun ModifierExample2(){
+    Column(
+        modifier= Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ){
+        Text(text="Hello world")
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+
+fun ModifierExample3() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Cyan)
+            .border(width = 2.dp, color = Color.Green)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+
+    }
+}
+
+@Preview( showBackground = true)
+@Composable
+fun CustomText(){
+    Column{
+        Text(
+            stringResource(R.string.hello_word_text),
+            color = colorResource(R.color.purple_700),
+            fontSize=28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+        val gradientColors = listOf(Cyan, Blue)
+        Text(
+            stringResource(R.string.hello_word_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+@Preview( showBackground = true)
+@Composable
+fun Picture(){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color.Black)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxHeight(),
+            painter = painterResource(R.drawable.Leon),
+            contentDescription = "Logo Android")
+        contentScale = ContentScale.Crop
+    }
+}
+
+
+
+
+
+fun clickAction(){
+    println("Column Clicked")
 }
