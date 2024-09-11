bb35b3d34639aed60bdb755eff4e6c0f468148e7
LunaSimonnie <lunasimonne@gmail.com>
2024-09-10 21:58:53 -0600

    Actividad10/09/24

--- app/src/main/java/com/example/proyect1/MainActivity.kt
+++ app/src/main/java/com/example/proyect1/MainActivity.kt
@@ -10,13 +10,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,30 +39,39 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
+import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.proyect1.ui.theme.Proyect1Theme
+//import com.example.proyect1.ui.theme.Proyect1Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
 
             Column(
-                modifier= Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
-            ){
-                Text(text ="Simple text")
-                ModifierExample()
-                ModifierExample2()
-                ModifierExample3()
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CustomText()
+                Picture()
+                Content1()
 
+
+                // Text(text ="Simple text")
+                // ModifierExample()
+                //ModifierExample2()
+                //  ModifierExample3()
             }
             //Layouts
             /*Column {
@@ -82,7 +101,6 @@ private fun column(function: () -> Unit) {
 }
 
 
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -91,73 +109,65 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+//@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Proyect1Theme {
-        Greeting("Android")
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun ModifierExample(){
+fun ModifierExample() {
     Column(
-        modifier= Modifier
+        modifier = Modifier
             .padding(24.dp)
-    ){
-        Text(text="Hello world")
+    ) {
+        Text(text = "Hello world")
 
     }
 }
-@Preview(showBackground = true)
-@Composable
 
-fun ModifierExample2(){
+//@Preview(showBackground = true)
+@Composable
+fun ModifierExample2() {
     Column(
-        modifier= Modifier
+        modifier = Modifier
             .padding(24.dp)
             .fillMaxWidth()
             .clickable(onClick = { clickAction() })
-    ){
-        Text(text="Hello world")
+    ) {
+        Text(text = "Hello world")
 
     }
 }
-@Preview(showBackground = true)
-@Composable
 
+//@Preview(showBackground = true)
+@Composable
 fun ModifierExample3() {
     Column(
         modifier = Modifier
             .fillMaxHeight()
             .padding(16.dp)
-            .background(Color.Cyan)
+            .background(Color.Red)
             .border(width = 2.dp, color = Color.Green)
             .width(200.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text(text = "Item 1")
-        Text(text = "Item 2")
-        Text(text = "Item 3")
-        Text(text = "Item 4")
-        Text(text = "Item 5")
-
+        Text(text = "Hello Item 1")
+        Text(text = "Hello Item 2")
+        Text(text = "Hello Item 3")
+        Text(text = "Hello Item 4")
+        Text(text = "Hello Item 5")
     }
 }
 
-@Preview( showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun CustomText(){
-    Column{
+fun CustomText() {
+    Column {
         Text(
             stringResource(R.string.hello_word_text),
-            color = colorResource(R.color.purple_700),
-            fontSize=28.sp,
-            fontStyle = FontStyle.Italic,
+            color = colorResource(R.color.purple_500),
+            fontSize = 28.sp,
             fontWeight = FontWeight.ExtraBold
         )
-        val gradientColors = listOf(Cyan, Blue)
+        val gradientColors = listOf(Cyan, Blue, Color.Red)
         Text(
             stringResource(R.string.hello_word_text),
             style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
@@ -165,27 +175,106 @@ fun CustomText(){
     }
 }
 
-@Preview( showBackground = true)
+@Preview(showBackground = true)
 @Composable
-fun Picture(){
+fun Picture() {
     Column(
         modifier = Modifier
-            .fillMaxHeight()
+            .fillMaxWidth()
             .background(Color.Black)
-    ){
+    ) {
         Image(
             modifier = Modifier
-                .fillMaxHeight(),
-            painter = painterResource(R.drawable.Leon),
-            contentDescription = "Logo Android")
-        contentScale = ContentScale.Crop
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.leon),
+            contentDescription = "Logo Android",
+            contentScale = ContentScale.Crop
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content1() {
+    Card(
+        modifier = Modifier
+            .background(Cyan)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(
+            text = "This is a Title",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            painter = painterResource(id = R.drawable.leon),
+            contentDescription = "Logo Android",
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            stringResource(R.string.Text_Card),
+            textAlign = TextAlign.Justify,
+            lineHeight = 18.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun BoxExample1(){
+    Box(
+        modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+        Image(painterResource(R.drawable.leon),
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.FillBounds
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp,150.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Filled.AccountCircle,
+                contentDescription = "Icon"
+            )
+            Text(text = "Text")
+        }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun BoxExample2(){
+    Box(
+        modifier = Modifier
+            .background(Color.Magenta)
+            .padding(5.dp)
+            .size(250.dp)
+    ) {
+        Text(text = "TopStart", Modifier.align(Alignment.TopStart))
+        Text(text = "TopEnd", Modifier.align(Alignment.TopEnd))
+        Text(text = "CenterStart", Modifier.align(Alignment.CenterStart))
+        Text(text = "Center", Modifier.align(Alignment.Center))
+        Text(text = "CenterEnd", Modifier.align(Alignment.CenterEnd))
+        Text(text = "BottomStart", Modifier.align(Alignment.BottomStart))
+        Text(text = "BottomEnd", Modifier.align(Alignment.BottomEnd))
+    }
+}
 
-
-
-
-fun clickAction(){
+fun clickAction() {
     println("Column Clicked")
+
 }
